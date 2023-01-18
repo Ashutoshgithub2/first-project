c726de77e0db3828f9168273bfda92ae3ae015f0
Ashutosh Kumar Ray <115968012+Ashutoshgithub2@users.noreply.github.com>
2023-01-18 10:56:47 +0530

    Add files via upload

---
+++ PROJECT.py
@@ -0,0 +1,29 @@
+def is_prime(n):
+    num = n
+    flag = False
+    if num == 1:
+        print(num, "is not prime number")
+    elif num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                flag = True
+                break
+        if flag:
+            print(num, "is composite number")
+            return 1
+        else:
+            print(num, "is prime number")
+            return 2
+a,b = input().split(",")
+pcount=0
+ccount=0
+for i in range(int(a), int(b) + 1):
+    p=is_prime(i)
+    if p==1:
+        ccount+=1
+    else:
+        pcount+=1
+print("count:",pcount,"prime and",ccount,"composite numbers in the range")
+#Name=Ashutosh Kumar Ray
+#Reg-No=12223004
+#Roll-N0=17
